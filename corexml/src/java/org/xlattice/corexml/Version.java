0930671240147347c797839a103c4c67534f4a2b
Jim Dixon <jddixon@gmail.com>
2013-12-31 16:34:55 -0800

    corexml after reconcileJava run

--- corexml/src/java/org/xlattice/corexml/Version.java
+++ corexml/src/java/org/xlattice/corexml/Version.java
@@ -2,17 +2,15 @@
 package org.xlattice.corexml;
 
 /**
- * DO NOT EDIT - THIS CLASS HAS BEEN AUTOMATICALLY GENERATED
- *
- * @author Jim Dixon
+ * THIS CLASS IS AUTOMATICALLY GENERATED.  DO NOT EDIT
  */
 public class Version extends org.xlattice.Version {
 
     public Version () {
-        super ("org.xlattice.corexml",  // package
-                   0,         // major
-                   3,         // minor
-                   7,         // decimal
-                   0);         // build
+        super("org.xlattice.corexml", // package
+            0,                       // minor
+            3,                       // minor
+            8,                       // decimal
+            0);                      // build
     }
-}
+};
