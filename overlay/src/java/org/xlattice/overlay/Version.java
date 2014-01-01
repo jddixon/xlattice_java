aba0187ed34881f12a6ce5063b5434a1767b88dc
Jim Dixon <jddixon@gmail.com>
2013-12-31 16:47:00 -0800

    overlay after reconcileJava run

--- overlay/src/java/org/xlattice/overlay/Version.java
+++ overlay/src/java/org/xlattice/overlay/Version.java
@@ -2,17 +2,15 @@
 package org.xlattice.overlay;
 
 /**
- * DO NOT EDIT - THIS CLASS HAS BEEN AUTOMATICALLY GENERATED
- *
- * @author Jim Dixon
+ * THIS CLASS IS AUTOMATICALLY GENERATED.  DO NOT EDIT
  */
 public class Version extends org.xlattice.Version {
 
     public Version () {
-        super ("org.xlattice.overlay",  // package
-                   0,         // major
-                   2,         // minor
-                   1,         // decimal
-                   0);        // build
+        super("org.xlattice.overlay", // package
+            0,                       // minor
+            2,                       // minor
+            2,                       // decimal
+            0);                      // build
     }
-}
+};
