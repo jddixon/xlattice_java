f438658da3a71d06a3b6da57ad40b29f5aed679d
Jim Dixon <jddixon@gmail.com>
2013-12-31 16:57:34 -0800

    projmgr after reconcileJava run plus rebuilt jars

--- projmgr/src/java/org/xlattice/projmgr/Version.java
+++ projmgr/src/java/org/xlattice/projmgr/Version.java
@@ -2,17 +2,15 @@
 package org.xlattice.projmgr;
 
 /**
- * THIS CLASS SHOULD BE AUTOMATICALLY GENERATED.
- *
- * @author Jim Dixon
+ * THIS CLASS IS AUTOMATICALLY GENERATED.  DO NOT EDIT
  */
 public class Version extends org.xlattice.Version {
 
     public Version () {
-        super ("org.xlattice.projmgr",  // package
-                0,                      // major
-                4,                      // minor
-                2,                      // decimal
-                0);                     // build
+        super("org.xlattice.projmgr", // package
+            0,                       // minor
+            4,                       // minor
+            2,                       // decimal
+            0);                      // build
     }
-}
+};
