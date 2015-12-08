6c73060e1e4e72125dd79120d380ef9024ba01ee
Jim Dixon <jddixon@gmail.com>
2015-12-08 12:45:32 -0800

    minor edits to overlay/build.xml

--- overlay/src/java/org/xlattice/overlay/Version.java
+++ overlay/src/java/org/xlattice/overlay/Version.java
@@ -8,9 +8,9 @@ public class Version extends org.xlattice.Version {
 
     public Version () {
         super("org.xlattice.overlay", // package
-            0,                       // minor
+            0,                       // major
             2,                       // minor
-            2,                       // decimal
+            4,                       // decimal
             0);                      // build
     }
 };
