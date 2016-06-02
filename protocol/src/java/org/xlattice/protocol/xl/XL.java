ccadc59159f4e434242bcb196a1e25f20a13ea3c
Jim Dixon <jddixon@gmail.com>
2016-06-01 17:02:15 -0700

    dropping subprojects

--- protocol/src/java/org/xlattice/protocol/xl/XL.java
+++
@@ -1,9 +0,0 @@
-/* XL.java */
-package org.xlattice.protocol.xl;
-
-public class XL                 implements org.xlattice.Protocol {
-
-    public final String name() {
-        return "xl";
-    }
-}
